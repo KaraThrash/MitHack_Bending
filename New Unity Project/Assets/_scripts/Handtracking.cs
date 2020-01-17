7d76effe1548286563f3f36327583899820c36d2
unknown <dvdonato@gmail.com>
2020-01-17 16:03:15 -0500

    hand controls

--- New Unity Project/Assets/_scripts/Handtracking.cs
+++ New Unity Project/Assets/_scripts/Handtracking.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Handtracking : MonoBehaviour
 {
   public int element; //air,earth,fire,water
   public Transform leftHand,rightHand;
   public Vector3 lastposLeft,lastposRight;
+  public List<GameObject> elements;//air,earth,fire,water
+  public List<Vector3> handMovements;//if the hand keeps moving continue to construct a more complex move
+  public GameObject heldElement;
+  public float elementspeed = 1.0f;
+  public float timer,distanceToCheckFor,timeIncrement = 0.2f;
+  public Text debugMovementDisplay;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +22,49 @@ public class Handtracking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      if(heldElement != null){
+
+          heldElement.transform.position = Vector3.MoveTowards(heldElement.transform.position, leftHand.transform.position, elementspeed * Time.deltaTime);
+          timer -= Time.deltaTime;
+          if(timer <= 0)
+          {
+            timer = timeIncrement;
+            HandMovementLogic();
+          }
+      }
+    }
+    public void HandMovementLogic()
+    {
+      if(Vector3.Distance(lastposLeft, leftHand.position) >= distanceToCheckFor)
+      {
+        //hand moved enough to not be hand shaking
+
+
+        //check left or right movement
+        string tempstring = "";
+        if(Mathf.Abs(lastposLeft.x - leftHand.position.x) >= distanceToCheckFor){Debug.Log("X");tempstring += "x";}
+        //check verticalmovement
+          if(Mathf.Abs(lastposLeft.y - leftHand.position.y) >= distanceToCheckFor){Debug.Log("Y");tempstring += "y";}
+          //check forward back movement
+            if(Mathf.Abs(lastposLeft.z - leftHand.position.z) >= distanceToCheckFor){Debug.Log("Z");tempstring += "z";}
+            lastposLeft = leftHand.transform.position;
+            debugMovementDisplay.text = tempstring;
+      }
+      else
+      {
+        //hand moved very little, or is just minor hand tremors that we want to normalize for
+        lastposLeft = leftHand.transform.position;
+        Debug.Log("NONE");
+        debugMovementDisplay.text = "None";
+      }
 
     }
     public void SetElement(int newElement)
     {
       element = newElement;
+      if(heldElement != null){Destroy(heldElement);}
+      heldElement = Instantiate(elements[element],leftHand.position,leftHand.rotation) as GameObject;
+      // heldElement.transform.parent = leftHand;
 
     }
 }
--- New Unity Project/Assets/_scripts/MouseControl.cs
+++ New Unity Project/Assets/_scripts/MouseControl.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class MouseControl : MonoBehaviour
 {
+  public float handspeed = 2.0f;
   public Transform hand;
-        public Vector3 targetpos;
+  public Vector3 targetpos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +17,19 @@ public class MouseControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      if(Input.GetKeyDown(KeyCode.A)){ GetComponent<Handtracking>().SetElement(0);}
+      if(Input.GetKeyDown(KeyCode.S)){ GetComponent<Handtracking>().SetElement(1);}
+      if(Input.GetKeyDown(KeyCode.D)){ GetComponent<Handtracking>().SetElement(2);}
+      if(Input.GetKeyDown(KeyCode.F)){ GetComponent<Handtracking>().SetElement(3);}
+    MoveHandWithMouse();
+    }
 
+    public void MoveHandWithMouse()
+    {
       targetpos = new Vector3(Mathf.Clamp(targetpos.x + (Input.GetAxis("HortMouse") * 0.01f), -1, 1) ,Mathf.Clamp(targetpos.y + (Input.GetAxis("VertMouse")* 0.01f), -1, 1),targetpos.z);
-      if(Input.GetMouseButton(0)) {Debug.Log("leftmouse"); targetpos = new Vector3(targetpos.x,targetpos.y,Mathf.Clamp(targetpos.z + 0.3f, -1, 1) ); }
-      if(Input.GetMouseButton(1)) {Debug.Log("rightmouse"); targetpos = new Vector3(targetpos.x,targetpos.y,Mathf.Clamp(targetpos.z - 0.3f, -1, 1)); }
-      hand.localPosition = Vector3.MoveTowards(hand.localPosition, targetpos, 1.0f * Time.deltaTime);
+      if(Input.GetMouseButton(0)) {Debug.Log("leftmouse"); targetpos = new Vector3(targetpos.x,targetpos.y,Mathf.Clamp(targetpos.z - 0.1f, -1, 1) ); }
+      if(Input.GetMouseButton(1)) {Debug.Log("rightmouse"); targetpos = new Vector3(targetpos.x,targetpos.y,Mathf.Clamp(targetpos.z + 0.1f, -1, 1)); }
+      hand.localPosition = Vector3.MoveTowards(hand.localPosition, targetpos, handspeed * Time.deltaTime);
 
-      // }
     }
 }
