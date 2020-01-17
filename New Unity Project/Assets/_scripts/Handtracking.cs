9cf8bedc38c8961cec19784eed432e6d056c0abc
unknown <dvdonato@gmail.com>
2020-01-17 13:04:35 -0500

    vive sdk

--- New Unity Project/Assets/_scripts/Handtracking.cs
+++ New Unity Project/Assets/_scripts/Handtracking.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Handtracking : MonoBehaviour
 {
+  public int element; //air,earth,fire,water
+  public Transform leftHand,rightHand;
+  public Vector3 lastposLeft,lastposRight;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void SetElement(int newElement)
+    {
+      element = newElement;
+
     }
 }
